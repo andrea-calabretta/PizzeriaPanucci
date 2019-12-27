dfb25fff83a8de889ea02bd896226441d26cb597
antromeo <Mass24365091>
2019-12-27 14:55:01 +0100

    Cliente Test e Test Main

--- pizzeriapanucci/src/prova.java
+++ pizzeriapanucci/src/prova.java
@@ -1,5 +1,13 @@
 public class prova {
     public static void main(String[] args) {
-        System.out.println("Prova");
+        System.out.println("Test Main");
+
+
+        Cliente c= new Cliente("marco", "polo", "marcopolo@gmail.com");
+        System.out.println(c.getNome());
+        System.out.println(c.getCognome());
+        System.out.println(c.getEmail());
+
+
     }
 }
